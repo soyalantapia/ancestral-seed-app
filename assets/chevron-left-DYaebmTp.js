e3493563327817028079b786993592dad9804e22
soyalantapia <alannaimtapia@gmail.com>
2026-05-13 08:46:42 -0300

    Updates

---
+++ assets/chevron-left-DYaebmTp.js
@@ -0,0 +1,1 @@
+import{n as e}from"./jsx-runtime-wPaO85xm.js";var t=e(`ChevronLeft`,[[`path`,{d:`m15 18-6-6 6-6`,key:`1wnfg3`}]]);export{t};
---
+++ assets/chevron-right-Dai0AAPZ.js
@@ -0,0 +1,1 @@
+import{n as e}from"./jsx-runtime-wPaO85xm.js";var t=e(`ChevronRight`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
