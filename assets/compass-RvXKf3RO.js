2ba3c08dea21d10ce54981963917c9de7ff04a11
soyalantapia <alannaimtapia@gmail.com>
2026-05-21 14:44:37 -0300

    Updates

---
+++ assets/compass-RvXKf3RO.js
@@ -0,0 +1,1 @@
+import{n as e}from"./jsx-runtime-wPaO85xm.js";var t=e(`Compass`,[[`path`,{d:`m16.24 7.76-1.804 5.411a2 2 0 0 1-1.265 1.265L7.76 16.24l1.804-5.411a2 2 0 0 1 1.265-1.265z`,key:`9ktpf1`}],[`circle`,{cx:`12`,cy:`12`,r:`10`,key:`1mglay`}]]);export{t};
