c9eb263ac0aeca438e1d4e914e0f7fbc4a1a398a
soyalantapia <alannaimtapia@gmail.com>
2026-05-28 17:49:17 -0300

    Updates

--- assets/sticky-note-BhfohtC2.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-CLX-66gl.js";var t=e(`StickyNote`,[[`path`,{d:`M16 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V8Z`,key:`qazsjp`}],[`path`,{d:`M15 3v4a2 2 0 0 0 2 2h4`,key:`40519r`}]]);export{t};
