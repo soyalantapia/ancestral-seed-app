996273f428a4a48b3a0de3c08647bd99b0f772b7
soyalantapia <alannaimtapia@gmail.com>
2026-05-28 13:07:44 -0300

    Updates

---
+++ assets/sticky-note-Bah03YRQ.js
@@ -0,0 +1,1 @@
+import{t as e}from"./createLucideIcon-DQtW_6s7.js";var t=e(`StickyNote`,[[`path`,{d:`M16 3H5a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2V8Z`,key:`qazsjp`}],[`path`,{d:`M15 3v4a2 2 0 0 0 2 2h4`,key:`40519r`}]]);export{t};
