c7a1b0149365f3b19514ae08f5334bc35952c12f
soyalantapia <alannaimtapia@gmail.com>
2026-05-30 17:38:54 -0300

    Updates

--- assets/lastVisit-BPUEzeoj.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./middleware-Bj_FqWbI.js";var n=e()(t(e=>({lastVisitAt:null,markVisited:()=>e({lastVisitAt:new Date().toISOString()})}),{name:`ancestral-seed-last-visit-v1`}));export{n as t};
