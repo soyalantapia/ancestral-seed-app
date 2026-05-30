ad57919d8edc106c947b599008be7bc746874541
soyalantapia <alannaimtapia@gmail.com>
2026-05-30 17:35:41 -0300

    Updates

--- assets/chevron-right-BQecWPOf.js
+++
@@ -1,1 +0,0 @@
-import{t as e}from"./createLucideIcon-CLX-66gl.js";var t=e(`ChevronRight`,[[`path`,{d:`m9 18 6-6-6-6`,key:`mthhwq`}]]);export{t};
--- assets/lastVisit-CCshuXNV.js
+++
@@ -1,1 +0,0 @@
-import{n as e,t}from"./middleware-HU0GtQT3.js";var n=e()(t(e=>({lastVisitAt:null,markVisited:()=>e({lastVisitAt:new Date().toISOString()})}),{name:`ancestral-seed-last-visit-v1`}));export{n as t};
